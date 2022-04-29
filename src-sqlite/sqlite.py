fa4dea8a95927d95efe905acd90e8f514a1d841f
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-04-29 10:47:52 -0300

    Att

--- src-sqlite/sqlite.py
+++ src-sqlite/sqlite.py
@@ -1,3 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect()
+def create_db():
+    """
+    Utilizado para criação do bando de dados.
+    """
+    conn = sqlite3.connect('teste.db')
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    create_db()
+
